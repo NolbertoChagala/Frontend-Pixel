e1e5276e42d594ab85ba98efbba9610edbd794d1
NolbertoChagala <21393137@utcancun.edu.mx>
2025-06-23 20:43:24 -0500

    reCAPTCHA listo

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import { sendContactForm } from "../services/api";
 import "../styles/ContactForm.css";
 
-// Validaciones
 const isValidName = (name) => /^[a-zA-ZÁÉÍÓÚáéíóúÑñ\s]{3,}$/.test(name);
 const isValidPhone = (phone) => /^\d{10,15}$/.test(phone);
 const isValidMessage = (msg) => msg.trim().length >= 10;
@@ -15,13 +15,19 @@ export default function ContactForm() {
     Mensaje: "",
   });
 
+  const [captchaToken, setCaptchaToken] = useState(null);
   const [submitted, setSubmitted] = useState(false);
+  const recaptchaRef = useRef(null);
 
   const handleChange = (e) => {
     setForm({
       ...form,
       [e.target.name]: e.target.value,
     });
+  };
+
+  const handleCaptchaChange = (token) => {
+    setCaptchaToken(token);
   };
 
   const handleSubmit = async (e) => {
@@ -33,7 +39,7 @@ export default function ContactForm() {
     }
 
     if (!isValidPhone(form.Telefono)) {
-      alert("Teléfono inválido. Debe tener entre 10 y 15 dígitos numéricos.");
+      alert("Teléfono inválido. Debe tener entre 10 y 15 dígitos.");
       return;
     }
 
@@ -42,8 +48,13 @@ export default function ContactForm() {
       return;
     }
 
+    if (!captchaToken) {
+      alert("Por favor verifica que no eres un robot.");
+      return;
+    }
+
     try {
-      await sendContactForm(form);
+      await sendContactForm({ ...form, token: captchaToken });
       setSubmitted(true);
       setForm({
         Nombre_Completo: "",
@@ -51,7 +62,8 @@ export default function ContactForm() {
         Telefono: "",
         Mensaje: "",
       });
-
+      setCaptchaToken(null);
+      reCAPTCHA.current?.reset();
       setTimeout(() => setSubmitted(false), 4000);
     } catch (error) {
       console.error("Error al enviar el formulario", error);
@@ -95,6 +107,14 @@ export default function ContactForm() {
           onChange={handleChange}
           required
         />
+
+        {/* CAPTCHA visible */}
+        <ReCAPTCHA
+          sitekey="6LdcaGsrAAAAAIp9NYAqs9mMfkwRM3dYn0MaW9aR"
+          onChange={handleCaptchaChange}
+          ref={recaptchaRef}
+        />
+
         <button type="submit">Enviar</button>
       </form>
     </section>
