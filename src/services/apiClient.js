30f7445e7624ecc193617c8fc2a86bd61fe96128
NolbertoChagala <21393137@utcancun.edu.mx>
2025-07-04 01:42:13 -0500

    Entorno de Producción y de local

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const apiClient = axios.create({
   baseURL: API_URL,
@@ -9,13 +9,16 @@ export const apiClient = axios.create({
   },
 });
 
-// Interceptor para agregar token automáticamente en cada petición si existe
-apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+// Interceptor para agregar token automáticamente
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
